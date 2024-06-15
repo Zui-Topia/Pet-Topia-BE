5ecd43502194db57bfa067eedbcc7e5c81a35e07
yookyung Choi <ykc131719@gmail.com>
2024-06-15 11:16:13 +0900

    test : 테스트 코드 작성 - SampleMapper로 데이터베이스 연결 확인

---
+++ src/test/java/com/zuitopia/petopia/sample/DataSourceTests.java
@@ -0,0 +1,42 @@
+package com.zuitopia.petopia.sample;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import lombok.extern.log4j.Log4j;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:**/*-context.xml")
+@Log4j
+public class DataSourceTests {
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired 
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testConnection() {
+		try(SqlSession session = sqlSessionFactory.openSession();
+				Connection con = session.getConnection();){
+			log.info(session);
+			log.info(con);
+		} 
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
---
+++ src/test/java/com/zuitopia/petopia/sample/SampleMapperTests.java
@@ -0,0 +1,40 @@
+package com.zuitopia.petopia.sample;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.zuitopia.petopia.sample.dto.SampleVO;
+import com.zuitopia.petopia.sample.mapper.SampleMapper;
+
+import lombok.extern.log4j.Log4j;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:**/*-context.xml")
+@Log4j
+public class SampleMapperTests {
+	@Autowired
+	private SampleMapper sampleMapper;
+	
+	@Test
+	public void testGet() {
+		try {
+			List<SampleVO> list = sampleMapper.getList();
+			
+			assertEquals(2, list.size());
+		}
+		catch (Exception e) {
+			log.info("message : " + e.getMessage());
+			log.info(e.getStackTrace());
+		}
+	}
+	
+}
