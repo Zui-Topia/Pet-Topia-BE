db33bf44714fd6076d22b5938f1f9418c702a061
yookyung Choi <ykc131719@gmail.com>
2024-06-18 17:38:46 +0900

    feat : SampleController ResponseEntity BaseResponse 적용

--- src/main/java/com/zuitopia/petopia/sample/controller/SampleController.java
+++ src/main/java/com/zuitopia/petopia/sample/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.zuitopia.petopia.sample.controller;
 
 import com.zuitopia.petopia.dto.SampleVO;
+import com.zuitopia.petopia.mypage.dto.MyReservationDTO;
+import com.zuitopia.petopia.util.BaseResponse;
 import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -20,15 +23,25 @@ public class SampleController {
 
     @GetMapping("/get")
     @ResponseBody
-    public ResponseEntity<String> get() {
-        log.info("get 들어옴");
-        return new ResponseEntity<>("success", HttpStatus.OK);
+    public ResponseEntity<BaseResponse> get(@RequestParam Long userId) {
+        log.info("get 들어옴" + userId);
+        return ResponseEntity
+                .ok()
+                .body(BaseResponse.builder()
+                        .success(true)
+                        .data("완료")
+                        .build());
     }
 
     @PostMapping("/post")
     @ResponseBody
-    public ResponseEntity<String> post(@RequestBody SampleVO sampleVO) {
-        log.info(sampleVO.toString());
-        return new ResponseEntity<>("success", HttpStatus.OK);
+    public ResponseEntity<BaseResponse> post(@RequestBody SampleVO sampleVO) {
+        log.info("post 들어옴" + sampleVO.toString());
+        return ResponseEntity
+                .ok()
+                .body(BaseResponse.builder()
+                        .success(true)
+                        .data(sampleVO)
+                        .build());
     }
 }
