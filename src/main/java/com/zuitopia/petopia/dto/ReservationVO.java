be5eff4db3313970404c47f12d9f91a57bbabb49
yookyung Choi <ykc131719@gmail.com>
2024-06-19 02:05:43 +0900

    feat : ReservationVO 공용 생성

---
+++ src/main/java/com/zuitopia/petopia/dto/ReservationVO.java
@@ -0,0 +1,17 @@
+package com.zuitopia.petopia.dto;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class ReservationVO {
+    private Long reservationId;
+    private Long userId; // 예약한 유저 아이디
+    private Long branchId; // 예약 지점 아이디
+    private String reservationToken; // 예약 번호
+    private Date reservationDate; // 원하는 예약 날짜
+    private String reservationVisitTime; // 원하는 방문 날짜
+    private Date reservationCreateDate; //예약 생성 날짜
+    private int reservationPayment;
+    private int reservationDelete; // 예약 삭제 여부
+}
