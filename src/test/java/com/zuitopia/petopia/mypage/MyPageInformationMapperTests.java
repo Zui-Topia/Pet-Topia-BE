b3568342c6d53ca1fcb78ede0cb81d79f5782252
yookyung Choi <ykc131719@gmail.com>
2024-06-19 16:43:46 +0900

    test : myPageInformationMapper 테스트 코드 작성

---
+++ src/test/java/com/zuitopia/petopia/mypage/MyPageInformationMapperTests.java
@@ -0,0 +1,36 @@
+package com.zuitopia.petopia.mypage;
+
+import static org.junit.Assert.assertEquals;
+
+import com.zuitopia.petopia.mypage.dto.MyPagePetDTO;
+import com.zuitopia.petopia.mypage.dto.MyPageUserDTO;
+import com.zuitopia.petopia.mypage.mapper.MyPageInformationMapper;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:**/*-context.xml")
+@Log4j
+public class MyPageInformationMapperTests {
+    @Autowired
+    private MyPageInformationMapper myPageInformationMapper;
+
+    @Test
+    public void testGetMyPageUserDTO(){
+        MyPageUserDTO myPageUserDTO = myPageInformationMapper.getMyPageUserDTO(2);
+        assertEquals("zuitopia@naver.com", myPageUserDTO.getUserEmail());
+    }
+
+    @Test
+    public void testGetMyPagePetDTO(){
+        MyPagePetDTO myPagePetDTO = myPageInformationMapper.getMyPagePetDTO(2);
+        assertEquals("토피아", myPagePetDTO.getPetName());
+    }
+
+}
