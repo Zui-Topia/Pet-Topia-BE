de65e3138dc13f0064ad402b2640042cc62f0c94
Do Yeon <mju1025@naver.com>
2024-06-25 21:09:14 +0900

    comment : 설명에 관한 주석 처리

--- src/main/java/com/zuitopia/petopia/map/service/MapService.java
+++ src/main/java/com/zuitopia/petopia/map/service/MapService.java
@@ -6,7 +6,10 @@ import com.zuitopia.petopia.dto.PlaceVO;
 import java.util.List;
 
 public interface MapService {
+    // 지점의 층정보 가져오기
     List<MapVO> getListFloorMapId(int branchId);
+    // 지점/ 층에 대한 지도 정보 가져오기
     List<PlaceVO> getMapInfo(PlaceVO placeVo);
+    //검색 기능
     List<PlaceVO> getSearchInfo(int branchId);
 }
