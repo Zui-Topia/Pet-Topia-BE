e71eba9f30191ab3fcfda4029f903ed86c4a504c
EunchanJeong <jec65147@gmail.com>
2024-06-30 14:12:00 +0900

    comment : 설명 주석 작성

--- src/main/java/com/zuitopia/petopia/reservation/dto/ReservationInfoDTO.java
+++ src/main/java/com/zuitopia/petopia/reservation/dto/ReservationInfoDTO.java
@@ -2,7 +2,18 @@ package com.zuitopia.petopia.reservation.dto;
 
 import lombok.Data;
 
-
+/**
+ * 예약 정보를 담는 DTO 클래스
+ * @author 정은찬
+ * @since 2024.06.19
+ *
+ * <pre>
+ * 수정일             수정자                      수정내용
+ * ----------  ----------------    ---------------------------------
+ * 2024.06.20       정은찬               예약 날짜 데이터 타입 변경
+ * 2024.06.19       정은찬                      최초 생성
+ * </pre>
+ */
 @Data
 public class ReservationInfoDTO {
     private int userId;                     // 예약한 유저 아이디
